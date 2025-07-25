f0fd1da743768f3cdb499daebb853c6295a6187c
Raul Renshaw <raulrenshaw@gmail.com>
2025-07-24 22:01:08 -0300

    add: Usando @Configuration e @ConfigurationPropertions utilizando do aplications.propertions

--- src/main/java/dio/springboot/EnviarMensagens/SistemaMensagens.java
+++ src/main/java/dio/springboot/EnviarMensagens/SistemaMensagens.java
@@ -1,25 +1,22 @@
 package dio.springboot.EnviarMensagens;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
 
 @Component
 public class SistemaMensagens implements CommandLineRunner {
-    @Value("${nome:Noreply}")
-    private String nome;
-    @Value("${email}")
-    private String email;
-    @Value("${telefones}")
-    private List<Long> telefones;
-
+    @Autowired
+    private Remetente remetente;
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("Mensagem enviado por: " + nome
-        + "\nCom E-mail: " + email
-        + "\nCom telefones para contato: " + telefones);
+        System.out.println("Mensagem enviado por: " + remetente.getNome()
+        + "\nCom E-mail: " + remetente.getEmail()
+        + "\nCom telefones para contato: " + remetente.getTelefones());
 
         System.out.println("Seu cadastro foi aprovado");
     }
